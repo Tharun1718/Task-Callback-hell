367d6f74832a5d15c8549db997e22400451215c1
Tharun1718 <108926726+Tharun1718@users.noreply.github.com>
2022-08-10 21:23:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const result=document.querySelector("#result");
+
+setTimeout(()=>{
+    result.innerHTML=10;
+    setTimeout(()=>{
+        result.innerHTML=9;
+        setTimeout(()=>{
+            result.innerHTML=8;
+            setTimeout(()=>{
+                result.innerHTML=7;
+                setTimeout(()=>{
+                    result.innerHTML=6;
+                    setTimeout(()=>{
+                        result.innerHTML=5;
+                        setTimeout(()=>{
+                            result.innerHTML=4;
+                            setTimeout(()=>{
+                                result.innerHTML=3;
+                                setTimeout(()=>{
+                                    result.innerHTML=2;
+                                    setTimeout(()=>{
+                                        result.innerHTML=1;
+                                        setTimeout(()=>{
+                                            result.innerHTML="Happy Independence day";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+                },1000);
+        },1000);
+    },1000);
+},1000);
